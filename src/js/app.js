d169ab6862e2ee8fe1e0165d089c0ba98ce9005f
Mary Kalugina <marina.kalugina9900@gmail.com>
2023-03-15 15:10:07 +0400

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,9 @@ import read from './reader';
 export default class GameSavingLoader {
   /* eslint-disable */load() {
     return read()
-      .then((data) => json(data));
+      .then((data) => json(data))
+      .catch(() => {
+        throw new Error('Ошибка');
+    });
   }
 }
